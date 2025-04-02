583ee94d0b15c6fe8572e9efee33c5a9555f38e1
Luis-Moura <luish.moura2024@gmail.com>
2025-04-02 20:50:47 -0300

    feat: ensure non-null values for environment variables in application configuration

--- src/main/java/com/tech_mel/tech_mel/TechMelApplication.java
+++ src/main/java/com/tech_mel/tech_mel/TechMelApplication.java
@@ -4,6 +4,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Objects;
+
 @SpringBootApplication
 public class TechMelApplication {
 
@@ -11,39 +13,39 @@ public class TechMelApplication {
         Dotenv dotenv = Dotenv.load();
 
         // Configurações do banco de dados
-        System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+        System.setProperty("spring.datasource.url", Objects.requireNonNull(dotenv.get("DB_URL")));
+        System.setProperty("spring.datasource.username", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+        System.setProperty("spring.datasource.password", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
 
         // Configuração do redis
-        System.setProperty("spring.data.redis.host", dotenv.get("REDIS_HOST"));
-        System.setProperty("spring.data.redis.port", dotenv.get("REDIS_PORT"));
-        System.setProperty("spring.data.redis.password", dotenv.get("REDIS_PASSWORD"));
-        System.setProperty("spring.data.redis.timeout", dotenv.get("REDIS_TIMEOUT"));
-        System.setProperty("spring.data.redis.lettuce.pool.max-active", dotenv.get("REDIS_POOL_MAX_TOTAL"));
-        System.setProperty("spring.data.redis.lettuce.pool.max-idle", dotenv.get("REDIS_POOL_MAX_IDLE"));
-        System.setProperty("spring.data.redis.lettuce.pool.min-idle", dotenv.get("REDIS_POOL_MIN_IDLE"));
+        System.setProperty("spring.data.redis.host", Objects.requireNonNull(dotenv.get("REDIS_HOST")));
+        System.setProperty("spring.data.redis.port", Objects.requireNonNull(dotenv.get("REDIS_PORT")));
+        System.setProperty("spring.data.redis.password", Objects.requireNonNull(dotenv.get("REDIS_PASSWORD")));
+        System.setProperty("spring.data.redis.timeout", Objects.requireNonNull(dotenv.get("REDIS_TIMEOUT")));
+        System.setProperty("spring.data.redis.lettuce.pool.max-active", Objects.requireNonNull(dotenv.get("REDIS_POOL_MAX_TOTAL")));
+        System.setProperty("spring.data.redis.lettuce.pool.max-idle", Objects.requireNonNull(dotenv.get("REDIS_POOL_MAX_IDLE")));
+        System.setProperty("spring.data.redis.lettuce.pool.min-idle", Objects.requireNonNull(dotenv.get("REDIS_POOL_MIN_IDLE")));
 
         // Configurações do OAuth2
-        System.setProperty("spring.security.oauth2.client.registration.google.client-id", dotenv.get("GOOGLE_CLIENT_ID"));
-        System.setProperty("spring.security.oauth2.client.registration.google.client-secret", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("spring.security.oauth2.client.registration.google.client-id", Objects.requireNonNull(dotenv.get("GOOGLE_CLIENT_ID")));
+        System.setProperty("spring.security.oauth2.client.registration.google.client-secret", Objects.requireNonNull(dotenv.get("GOOGLE_CLIENT_SECRET")));
 
         // Configurações do JWT
-        System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
-        System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION"));
-        System.setProperty("jwt.refresh-expiration", dotenv.get("JWT_REFRESH_EXPIRATION"));
+        System.setProperty("jwt.secret", Objects.requireNonNull(dotenv.get("JWT_SECRET")));
+        System.setProperty("jwt.expiration", Objects.requireNonNull(dotenv.get("JWT_EXPIRATION")));
+        System.setProperty("jwt.refresh-expiration", Objects.requireNonNull(dotenv.get("JWT_REFRESH_EXPIRATION")));
 
         // Configurações de email
-        System.setProperty("spring.mail.host", dotenv.get("MAIL_HOST"));
-        System.setProperty("spring.mail.port", dotenv.get("MAIL_PORT"));
-        System.setProperty("spring.mail.username", dotenv.get("MAIL_USERNAME"));
-        System.setProperty("spring.mail.password", dotenv.get("MAIL_PASSWORD"));
-        System.setProperty("spring.mail.from", dotenv.get("MAIL_FROM"));
-        System.setProperty("spring.mail.properties.mail.smtp.auth", dotenv.get("MAIL_AUTH"));
-        System.setProperty("spring.mail.properties.mail.smtp.starttls.enable", dotenv.get("MAIL_STARTTLS"));
+        System.setProperty("spring.mail.host", Objects.requireNonNull(dotenv.get("MAIL_HOST")));
+        System.setProperty("spring.mail.port", Objects.requireNonNull(dotenv.get("MAIL_PORT")));
+        System.setProperty("spring.mail.username", Objects.requireNonNull(dotenv.get("MAIL_USERNAME")));
+        System.setProperty("spring.mail.password", Objects.requireNonNull(dotenv.get("MAIL_PASSWORD")));
+        System.setProperty("spring.mail.from", Objects.requireNonNull(dotenv.get("MAIL_FROM")));
+        System.setProperty("spring.mail.properties.mail.smtp.auth", Objects.requireNonNull(dotenv.get("MAIL_AUTH")));
+        System.setProperty("spring.mail.properties.mail.smtp.starttls.enable", Objects.requireNonNull(dotenv.get("MAIL_STARTTLS")));
 
         // Perfil ativo
-        System.setProperty("spring.profiles.active", dotenv.get("SPRING_PROFILE"));
+        System.setProperty("spring.profiles.active", Objects.requireNonNull(dotenv.get("SPRING_PROFILE")));
 
         SpringApplication.run(TechMelApplication.class, args);
     }
