2729366b726815c7a4414cec34621b72c97cbbff
Luis-Moura <luish.moura2024@gmail.com>
2025-06-19 19:27:19 -0300

    refactor: remove unused imports in TechMelApplication for cleaner code

--- src/main/java/com/tech_mel/tech_mel/TechMelApplication.java
+++ src/main/java/com/tech_mel/tech_mel/TechMelApplication.java
@@ -1,10 +1,7 @@
 package com.tech_mel.tech_mel;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Objects;
 
 @SpringBootApplication
 public class TechMelApplication {
