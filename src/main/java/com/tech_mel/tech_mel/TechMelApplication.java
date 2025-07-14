e482b2dc370862bcda7b0b84c9b462cbeae1b0da
Luis-Moura <luish.moura2024@gmail.com>
2025-07-14 09:34:45 -0300

    feat: enable scheduling support in TechMelApplication

--- src/main/java/com/tech_mel/tech_mel/TechMelApplication.java
+++ src/main/java/com/tech_mel/tech_mel/TechMelApplication.java
@@ -2,8 +2,10 @@ package com.tech_mel.tech_mel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TechMelApplication {
     public static void main(String[] args) {
         SpringApplication.run(TechMelApplication.class, args);
