a50b696031f5d30208c68b8b875d8c4877bbaef1
Luis-Moura <luish.moura2024@gmail.com>
2025-07-14 09:34:26 -0300

    feat: update DailyMeasurementAverageEntity to use UUID generation strategy for ID

--- src/main/java/com/tech_mel/tech_mel/infrastructure/persistence/entity/DailyMeasurementAverageEntity.java
+++ src/main/java/com/tech_mel/tech_mel/infrastructure/persistence/entity/DailyMeasurementAverageEntity.java
@@ -17,6 +17,7 @@ import java.util.UUID;
 @Builder
 public class DailyMeasurementAverageEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false)
