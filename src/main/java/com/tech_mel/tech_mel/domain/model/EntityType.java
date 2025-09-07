add4b7f83a9dd3f823c6d194b5b486e6507907a1
Luis-Moura <luish.moura2024@gmail.com>
2025-09-07 10:57:09 -0300

    feat: add EntityType enum for domain model categorization

---
+++ src/main/java/com/tech_mel/tech_mel/domain/model/EntityType.java
@@ -0,0 +1,13 @@
+package com.tech_mel.tech_mel.domain.model;
+
+public enum EntityType {
+    USER,
+    HIVE,
+    MEASUREMENT,
+    ALERT,
+    THRESHOLD,
+    REFRESH_TOKEN,
+    DAILY_AVERAGE,
+    AUDIT_LOG,
+    SYSTEM_CONFIG
+}
